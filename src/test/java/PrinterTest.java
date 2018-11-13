718c2a39d1234a9a701a24d1089ebe92b8efaf72
GillianC <gccc0309@gmail.com>
2018-11-13 07:49:48 +0000

    renamed tests for consistency and updated assert to true and false for boolean check

--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,9 +1,13 @@
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 
 public class PrinterTest {
-    Printer printer;
+    private Printer printer;
     @Before
     public void before(){
         printer = new Printer();
@@ -12,12 +16,12 @@ public class PrinterTest {
     @Test
     public void printCanPrint(){
 
-        assertEquals(true, printer.print(1,1));
+        assertTrue("This didn't return true", printer.print(1,1));
         assertEquals(499, printer.getSheetsleft());
     }
     @Test
-    public void printCannotPrint(){
-        assertEquals(false, printer.print(100,100));
+    public void printCannotPrintPaperLow(){
+        assertFalse("This didn't return false", printer.print(100,100));
         assertEquals(500, printer.getSheetsleft());
     }
     @Test
@@ -29,14 +33,14 @@ public class PrinterTest {
     }
 
     @Test
-    public void willReduceTonerLevel(){
+    public void printCanPrintReduceTonerLevel(){
         printer.print(10,1);
         assertEquals(90, printer.getTonerLevel());
 
     }
 
     @Test
-    public void tonerLevelTooLow(){
+    public void printCannotPrintTonerLow(){
         printer.print(10,10);
         assertEquals(100, printer.getTonerLevel());
     }
